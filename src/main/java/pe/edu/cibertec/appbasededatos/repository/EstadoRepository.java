e16e2f367485cfc56b07e83bc01cf322542b5745
gaxel.18 <alejandro_cs_09@hotmail.com>
2023-04-30 12:00:43 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/appbasededatos/repository/EstadoRepository.java
@@ -0,0 +1,10 @@
+package pe.edu.cibertec.appbasededatos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pe.edu.cibertec.appbasededatos.model.bd.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+		
+	}
+
